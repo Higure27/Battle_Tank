1190bf05d06fa6ce7149543896defc56f6b07433
Or Rahav <kevan.or@gmail.com>
2020-07-16 00:40:47 -0600

    Updated RelativeRotation to GetRelativeRotation function to be compatibe with UE4.25

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -7,7 +7,7 @@ void UTankBarrel::Elevate(float relativeSpeed)
 	relativeSpeed = FMath::Clamp(relativeSpeed, -1.f, 1.f);
 	// Framerate Independent by using delta 
 	float elevationChange = relativeSpeed * maxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
-	float newElevation = RelativeRotation.Pitch + elevationChange;
+	float newElevation = GetRelativeRotation().Pitch + elevationChange;
 	float clampedNewElevation = FMath::Clamp(newElevation, minElevationDegrees, maxElevationDegrees);
 	//UE_LOG(LogTemp, Warning, TEXT("Found aim solution, New elevation is %f "), clampedNewElevation);
 	SetRelativeRotation(FRotator(clampedNewElevation,0,0));
--- BattleTank/Source/BattleTank/Private/TankTurret.cpp
+++ BattleTank/Source/BattleTank/Private/TankTurret.cpp
@@ -7,7 +7,7 @@ void UTankTurret::Rotate(float relativeSpeed)
 	relativeSpeed = FMath::Clamp(relativeSpeed, -1.f, 1.f);
 	//Framerate Independent by using delta 
 	float rotationChange = relativeSpeed * maxDegreesPerSecond* GetWorld()->DeltaTimeSeconds;
-	float newRotation = RelativeRotation.Yaw + rotationChange;
+	float newRotation = GetRelativeRotation().Yaw + rotationChange;
 	SetRelativeRotation(FRotator(0, newRotation, 0));
 }
 
